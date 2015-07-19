c7d67c2ca05789c6c5f7ed349fcd7c6e66439836
Nikita Nyatin <yesnik@yandex.ru>
2015-07-19 21:17:17 +0500

    Update 03-include-method.js

--- inheritance/03-include-method.js
+++ inheritance/03-include-method.js
@@ -28,3 +28,25 @@ Person.save(4); //TypeError: Person.save is not a function
 //Метод save доступен из экземпляра класса
 var leo = new Person('Leo');
 leo.save(7); //Saving item #7
+
+//Удобно выделить реализацию метода save в отдельный модуль,
+//который можно будет подключать к классам
+var ORMModule = {
+  save: function (id) {
+    console.log('Saving item #' + id);
+  }
+}
+
+var Car = new Class;
+var Bike = new Class;
+
+//Добавляем к обоим классам методы объекта ORMModule
+//Все экземпляры этих классов будут метод save()
+Car.include(ORMModule);
+Bike.include(ORMModule);
+
+var bmw = new Car;
+bmw.save(5); //Saving item #5
+
+var merida = new Bike;
+merida.save(8); //Saving item #5
