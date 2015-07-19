b8ec244865416e35d9ba75a49c12d4607f775611
Nikita Nyatin <yesnik@yandex.ru>
2015-07-19 14:32:43 +0500

    Update 03-include-method.js

--- inheritance/03-include-method.js
+++ inheritance/03-include-method.js
@@ -1,21 +1,16 @@
 var Class = function () {
-  //Создаем ф-цию, в которой будем запускать метод
-  //init с аргументами, переданными в Class(args)
-  //Используем 'klass', т.к. слово 'class' уже зарезервировано в js.
   var klass = function () {
     this.init.apply(this, arguments);
   }
-  //Определяем, что init, иначе - ошибка, если
-  //юзер не определит этот метод в своем классе
   klass.prototype.init = function(){};
   
   //Метод для добавления свойств к экземпляру класса
   klass.include = function (obj) {
+    //Перебираем все свойства переданного obj и добавляем их к прототипу класса
     for (var prop in obj) {
       klass.prototype[prop] = obj[prop];
     }
   }
-  
   return klass;
 }
 
@@ -27,9 +22,9 @@ Person.include({
   }
 });
 
-//Нельзя вызвать метод save через класс Person
+//Метод save нельзя вызвать через класс Person
 Person.save(4); //TypeError: Person.save is not a function
 
-//Можно метод save вызвать через экземпляр класса Person
+//Метод save можно вызвать через экземпляр класса Person
 var leo = new Person('Leo');
 leo.save(7); //Saving item #7
