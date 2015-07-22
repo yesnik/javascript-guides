e1d38e3a86b931ab9741a38deaa9c571a4db6507
Nikita Nyatin <yesnik@yandex.ru>
2015-07-22 18:59:25 +0500

    Create 05-inheritance-to-lib.js

---
+++ inheritance/05-inheritance-to-lib.js
@@ -0,0 +1,20 @@
+var Class = function (parent) {
+  var klass = function () {
+    this.init.apply(this, arguments);
+  }
+  klass.prototype.init = function () {};
+  
+  //Изменяем прототип объекта klass, чтобы он указывал на родительский объект,
+  //свойства которого мы хотим унаследовать
+  if (parent) {
+    //Создаем новую функцию-конструктор
+    var subclass = function() {};
+    //Делаем ей прототип родительского объекта
+    subclass.prototype = parent.prototype;
+    //Экземпляр полученного объекта делаем прототипом для klass
+    klass.prototype = new subclass;
+  };
+  
+  //Методы include, extend...
+  return klass;
+}
