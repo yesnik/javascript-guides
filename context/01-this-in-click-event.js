0032b4878d86aa6474600174a4a7031a7de80695
Nikita Nyatin <yesnik@yandex.ru>
2015-07-29 12:58:19 +0500

    Update 01-this-in-click-event.js

--- context/01-this-in-click-event.js
+++ context/01-this-in-click-event.js
@@ -1,4 +1,4 @@
-//Пример 1.1. Обращение к исходному объекту через вспомогательную переменную self
+//Пример 1.1. Передаем функцию-обработчик напрямую
 var App = {
   name: 'Johny',
   titleClicked: function () {
@@ -6,19 +6,61 @@ var App = {
     console.log('this.name: ' + this.name);
   },
   init: function () {
-    var self = this;
     //Передаем в click() функцию-обработчик напрямую
-    $('#page_title').click(self.titleClicked);
+    $('#page_title').click(this.titleClicked);
   }
 };
 
 App.init();
 //При клике на элемент:
 // this:  <h2 id="page_title">
-// this.name: undefined
+// this.name: undefined, т.к. ф-ция self.titleClicked вызвана в контексте элемента h2.
 
 
-//Пример 1.2. Обращение к исходному объекту по его имени - App
+//Пример 1.2. Используем bind(), чтобы указать контекст, в котором будет вызываться функция-обработчик
+var App = {
+  name: 'Johny',
+  titleClicked: function () {
+    console.log(this);
+    console.log('this.name: ' + this.name);
+  },
+  init: function () {
+    $('#page_title').click(this.titleClicked.bind(this));
+  }
+};
+
+App.init();
+//При клике на элемент:
+//this:  Object { name="Johny",  titleClicked=function(),  init=function()}
+//this.name: Johny
+
+
+//Пример 2.1. Вызов функции-обработчика внутри анонимной функции.
+//            Используем self, чтобы получить доступ к исходному объекту App
+var App = {
+  name: 'Johny',
+  titleClicked: function () {
+    console.log(this);
+    console.log('this.name: ' + this.name);
+  },
+  init: function () {
+    //Используем промежуточную переменную, чтобы сохранить ссылку на сам объект App
+    var self = this;
+    //Передаем в click() анонимную функцию, в которой вызываем функцию-обработчик
+    $('#page_title').click(function () {
+      self.titleClicked();
+    });
+  }
+};
+
+App.init();
+//При клике на элемент:
+//this: Object { name="Johny",  titleClicked=function(),  init=function()}
+//this.name: Johny
+
+
+//Пример 2.2. Вызов функции-обработчика внутри анонимной функции. 
+//            Обращение к исходному объекту по его имени - App
 var App = {
   name: 'Johny',
   titleClicked: function () {
@@ -37,26 +79,4 @@ var App = {
 App.init();
 //При клике на элемент:
 //this:  Object { name="Johny",  titleClicked=function(),  init=function()}
-//this.name: Johny
-
-
-//Пример 2. 
-var App = {
-  name: 'Johny',
-  titleClicked: function () {
-    console.log(this);
-    console.log('this.name: ' + this.name);
-  },
-  init: function () {
-    var self = this;
-    //Передаем в click() анонимную функцию, в которой вызываем функцию-обработчик
-    $('#page_title').click(function () {
-      self.titleClicked();
-    });
-  }
-};
-
-App.init();
-//При клике на элемент:
-//this: Object { name="Johny",  titleClicked=function(),  init=function()}
 //this.name: Johny
